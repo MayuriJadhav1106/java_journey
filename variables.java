6e97a1f4fbfffb8d8635be88fee541617744f2b9
MayuriJadhav1106 <mayurijadhav1106@gmail.com>
2024-04-09 14:15:04 +0530

    created a variables.java file

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 15;
+        System.out.println("a");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("b");
+
+        a=20;
+        System.out.println(a);
+    }
+}
