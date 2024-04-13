5dd657199321aa9133c5ae36a4b8a94c6addcfd8
MayuriJadhav1106 <mayurijadhav1106@gmail.com>
2024-04-13 22:41:04 +0530

    created a largestNo.java file

---
+++ conditional_statement/largestNo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class largestNo{
+    public static void main(String args []){
+        int A = 1;
+        int B = 5;
+        int C = 2;
+        if(A >= B && A >= C)
+        {
+            System.out.println("A is greater");
+        }
+        else if(B >= C)
+        {
+           System.out.println("B is greater");
+        }
+        else
+        {
+            System.out.println("C is greater");
+        }
+        
+    }
+}
