ce5d18991b71ae7393820e457bc2b27586b05412
MayuriJadhav1106 <mayurijadhav1106@gmail.com>
2024-04-14 22:21:27 +0530

    created a switchcase.java file

---
+++ conditional_statement/switchcase.java
@@ -0,0 +1,14 @@
+public class switchcase{
+    public static void main(String args[]){
+        int number = 2;
+        switch(number){
+            case 1 : System.out.println("red");
+                     break;
+            case 2 : System.out.println("blue");
+                     break;
+            case 3 : System.out.println("white");
+                     break;
+            default : System.out.println("no color");
+        }
+    }
+}
