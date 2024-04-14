81c3d35f6681229d8122f7b135a1eed951c56311
MayuriJadhav1106 <mayurijadhav1106@gmail.com>
2024-04-14 22:03:18 +0530

    a little change in code

--- conditional_statement/passfail.java
+++ conditional_statement/passfail.java
@@ -1,7 +1,7 @@
 public class passfail{
     public static void main(String args[]){
         int marks = 35;
-        String type = (marks >= 35) ? "pass" : "fail";
-        System.out.println(type);
+        String result = (marks >= 35) ? "pass" : "fail";
+        System.out.println(result);
     }
 }
