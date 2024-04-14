8227586e22f0ba306aad1dd3cba179cac4149445
MayuriJadhav1106 <mayurijadhav1106@gmail.com>
2024-04-14 22:02:41 +0530

    created a passfail.java file

---
+++ conditional_statement/passfail.java
@@ -0,0 +1,7 @@
+public class passfail{
+    public static void main(String args[]){
+        int marks = 35;
+        String type = (marks >= 35) ? "pass" : "fail";
+        System.out.println(type);
+    }
+}
